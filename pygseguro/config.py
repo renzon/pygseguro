53e02104bebc4dff881970a400f84f5712f74efb
renzon <renzon@gmail.com>
2019-03-26 01:08:07 -0300

    Implementada versão inicial de framework de configuração
    close #13

--- pygseguro/config.py
+++ pygseguro/config.py
@@ -1,18 +1,81 @@
 """
-Esse módulo contém a configuração padrão das chamadas e fornece uma classe de Config que representa essa configuração
+Esse módulo contém a configuração padrão das chamadas e fornece uma classe de ConfigConta que representa essa
+configuração
+
+Doc da API: https://dev.pagseguro.uol.com.br/reference#autenticacao
 """
 
 config_padrao = None
 
+PRODUCAO = 'https://ws.pagseguro.uol.com.br'
 
-class Config:
+
+class Config():
+    def __init__(self, ambiente: str):
+        self.ambiente = ambiente
+
+    def ambiente_endpoint(self, endpoint: str) -> str:
+        """
+        Constroi o {schema}://{ambiente}/{endpoint} para o pagseguro
+        :param endpoint: endpoint para onde será enviada a requisição
+        :return: base da url para o endpoint
+        """
+        return f'{self.ambiente}{endpoint}'
+
+    def query_string(self) -> str:
+        """
+        Metodo abstrato que deve ser implementado em cada subclasse.
+        Deve retornar a query string que deve ser enviada para autenticao no endpoint
+        :return: query string
+        """
+        raise NotImplementedError()
+
+    def construir_url(self, endpoint: str) -> str:
+        """
+        Método que constroi a url para completa para envio de requisição para a API
+        :param endpoint:
+        :return:
+        """
+        return f'{self.ambiente_endpoint(endpoint)}?{self.query_string()}'
+
+
+class ConfigConta(Config):
     """
-    Classe que representa uma configuração
+    Classe que representa uma configuração por email e token
     """
 
-    def __init__(self, email=None, token=None):
+    def __init__(self, email: str, token: str, ambiente: str = PRODUCAO):
+        super().__init__(ambiente=ambiente)
         self.token = token
         self.email = email
 
-    def __repr__(self):
-        return f'Config(email={self.email!r}, token={self.token!r})'
+    def __repr__(self) -> str:
+        return f'ConfigConta(email={self.email!r}, token={self.token!r})'
+
+    def query_string(self) -> str:
+        """
+        Constroi query string contendo email e token
+        :return:
+        """
+        return f'email={self.email}&token={self.token}'
+
+
+class ConfigApp(Config):
+    """
+    Classe que representa uma configuração por app_id e app_key
+    """
+
+    def __init__(self, app_id: str, app_key: str, ambiente: str = PRODUCAO):
+        super().__init__(ambiente)
+        self.app_key = app_key
+        self.app_id = app_id
+
+    def __repr__(self) -> str:
+        return f'ConfigApp(app_id={self.app_id!r}, app_key={self.app_key!r})'
+
+    def query_string(self) -> str:
+        """
+        Constroi query string contendo app_key e app_id
+        :return:
+        """
+        return f'appID={self.app_id}&appKey={self.app_key}'
--- pygseguro/tests/test_config.py
+++ pygseguro/tests/test_config.py
@@ -3,16 +3,38 @@ Módulo destinado a testar as configurações do projeto
 """
 import pytest
 
-from pygseguro import Config, apagar_config_padrao, get_config_padrao, set_config_padrao
+from pygseguro import ConfigApp, ConfigConta, apagar_config_padrao, get_config_padrao, set_config_padrao
 
 
 @pytest.fixture
-def cfg():
-    return Config(email='foo@bar.com', token='blah')
+def cfg_conta() -> ConfigConta:
+    return ConfigConta(email='foo@bar.com', token='blah')
 
 
-def test_config_padrao(cfg):
+@pytest.fixture
+def cfg_app() -> ConfigApp:
+    return ConfigApp(app_id='1234', app_key='segredo')
+
+
+def test_config_padrao(cfg_conta: ConfigConta):
+    """Cecha possibilidade de configuração global"""
     apagar_config_padrao()
     assert get_config_padrao() is None
-    set_config_padrao(cfg)
-    assert get_config_padrao() is cfg
+    set_config_padrao(cfg_conta)
+    assert get_config_padrao() is cfg_conta
+
+
+endpoints = pytest.mark.parametrize('endpoint', '/comprar /checkout /notificaes'.split())
+
+
+@endpoints
+def test_config_conta_url_producao(cfg_conta: ConfigConta, endpoint: str):
+    """Testa geração de url apontado para ambiente de produção por padrão e autentação de conta"""
+    assert cfg_conta.construir_url(
+        endpoint) == f'https://ws.pagseguro.uol.com.br{endpoint}?email=foo@bar.com&token=blah'
+
+
+@endpoints
+def test_config_app_url_producao(cfg_app: ConfigApp, endpoint: str):
+    """Testa geração de url apontado para ambiente de produção por padrão e autentação de app"""
+    assert cfg_app.construir_url(endpoint) == f'https://ws.pagseguro.uol.com.br{endpoint}?appID=1234&appKey=segredo'
